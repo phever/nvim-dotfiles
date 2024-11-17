eb1f89f87c4af79b5baaf2a3e6938df6126554ca
phever <3782319+phever@users.noreply.github.com>
2024-11-16 17:25:57 -0700

    init

---
+++ lua/chadrc.lua
@@ -0,0 +1,47 @@
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- Please read that file to know all available options :(
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+  theme = "chadracula",
+  theme_toggle = { "chadracula", "one_light" },
+
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+M.nvdash = {
+  load_on_startup = true,
+
+  header = {
+    "     What shall we do today?    ",
+    "",
+    "                (`.        ,-,  ",
+    "                ` `.    ,;' /   ",
+    "                 `.  ,'/ .'     ",
+    "                  `. X /.'      ",
+    "         .-;--''--.._` ` (      ",
+    "       .'            /   `      ",
+    "     ,           ` '   Q '      ",
+    "     ,         ,   `._    \\     ",
+    "  ,.|         '     `-.;_'      ",
+    "  :  . `  ;    `  ` --,.._;     ",
+    "   ' `    ,   )   .'            ",
+    "       `._ ,  '   /_            ",
+    "          ; ,''-,;' ``-         ",
+    "           ``-..__``--`         ",
+  },
+  --
+  -- buttons = {
+  --   { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+  --   { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+  --   -- more... check nvconfig.lua file for full list of buttons
+  -- },
+}
+
+return M
---
+++ lua/plugins/init.lua
@@ -0,0 +1,78 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "python",
+        "typescript",
+        "javascript",
+      },
+    },
+  },
+
+  "nvim-lua/plenary.nvim",
+
+  {
+    "nvchad/ui",
+    config = function()
+      require "nvchad"
+    end,
+  },
+
+  {
+    "nvchad/base46",
+    lazy = true,
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
+
+  "nvchad/volt", -- optional, needed for theme switcher
+
+  -- python debug adapters
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      require "configs.dap"
+    end,
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      require "configs.dap-ui"
+    end,
+  },
+
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function()
+      require "configs.dap-python"
+    end,
+  },
+}
