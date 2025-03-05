d677c95ee3d734625493278bb2b621bb837cd079
phever <3782319+phever@users.noreply.github.com>
2025-03-05 02:48:22 -0700

    add back to servers for masoninstall

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "basedpyright", "ruff", "tailwindcss", "emmet_language_server" }
+local servers = { "cssls", "basedpyright", "ruff", "tailwindcss", "emmet_language_server", "angularls", "vtsls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
