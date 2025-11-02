1fa7e80ea82bb3324bd0a39c29d82f8383712231
phever <3782319+phever@users.noreply.github.com>
2025-11-01 18:06:14 -0600

    add <leader>X close all other buffers shortcut

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>X", ":%bd|e#<cr>", { desc = "Close all buffers except current" })
 
 -- { "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
 -- { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
