525252510a519b7dac5411d5e2191ef18d109f84
phever <3782319+phever@users.noreply.github.com>
2025-11-26 01:32:59 -0700

    update copilot preferences

--- lua/configs/copilot-chat.lua
+++ lua/configs/copilot-chat.lua
@@ -1,9 +1,9 @@
 local user = vim.env.USER or "User"
 user = user:sub(1, 1):upper() .. user:sub(2)
 return {
-  model = "claude-sonnet-4",
+  model = "gemini-3-pro-preview",
   temperature = "0.1",
-  -- auto_insert_mode = true,
+  auto_insert_mode = false,
   question_header = "  " .. user .. " ",
   answer_header = "  Copilot ",
   window = {
