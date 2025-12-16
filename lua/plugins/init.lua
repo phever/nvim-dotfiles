843ab9b4bc35ba4d412d29ac88e1825e2ff2ebf1
phever <3782319+phever@users.noreply.github.com>
2025-12-16 05:11:40 -0700

    add telescope + telescope-ui-selector

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -114,4 +114,37 @@ return {
       chat.setup(opts)
     end,
   },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+    opts = {
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {
+            -- even more opts
+          },
+
+          -- pseudo code / specification for writing custom displays, like the one
+          -- for "codeactions"
+          -- specific_opts = {
+          --   [kind] = {
+          --     make_indexed = function(items) -> indexed_items, width,
+          --     make_displayer = function(widths) -> displayer
+          --     make_display = function(displayer) -> function(e)
+          --     make_ordinal = function(e) -> string
+          --   },
+          --   -- for example to disable the custom builtin "codeactions" display
+          --      do the following
+          --   codeactions = false,
+          -- }
+        },
+      },
+    },
+    config = function()
+      require("telescope").load_extension "ui-select"
+    end,
+  },
 }
